0b9e4cf4f339f92eac6e6d82b1aba6dc0709f14a
Tim Field <field.tim@gmail.com>
2023-09-17 18:03:50 +1200

    v1 cleanup

--- src/app/chorus/image-list.tsx
+++
@@ -1,8 +0,0 @@
-import { getImages } from "./get-images"
-import { Images } from "./images"
-
-export const ImageList = async () => {
-  const images = await getImages()
-
-  return <Images images={images} />
-}
--- src/app/chorus/layout.tsx
+++
@@ -1,9 +0,0 @@
-import styles from "./layout.module.css"
-
-export default function ChorusLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
-  return <main className={styles.main}>{children}</main>
-}
